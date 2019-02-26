6323cf19a37bb83ffb050533e2f96955cd4549c2
Pandiarajan Palpandi <askalaveeska@gmail.com>
2019-02-26 23:37:23 +0530

    test commits

---
+++ store-api/src/main/java/com/aska/store/controller/LoginController.java
@@ -0,0 +1,7 @@
+package com.aska.store.controller;
+
+/**
+ * Created by askalaveeska on 26/02/19.
+ */
+public class LoginController {
+}
---
+++ store-api/src/main/java/com/aska/store/entity/ProductEntity.java
@@ -0,0 +1,7 @@
+package com.aska.store.entity;
+
+/**
+ * Created by askalaveeska on 26/02/19.
+ */
+public class ProductEntity {
+}
