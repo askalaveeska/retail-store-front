c8b082557b87e3577febc1eb2e6ada2ecc51bc23
Pandiarajan Palpandi <askalaveeska@gmail.com>
2019-02-26 23:46:30 +0530

    develop branch first commit

--- store-api/src/main/java/com/aska/store/controller/ProductController.java
+++ store-api/src/main/java/com/aska/store/controller/ProductController.java
@@ -1,7 +1,10 @@
 package com.aska.store.controller;
 
+import org.springframework.web.bind.annotation.RestController;
+
 /**
  * Created by askalaveeska on 26/02/19.
  */
+@RestController
 public class ProductController {
 }
